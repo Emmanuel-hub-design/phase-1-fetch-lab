2af760640b44abe407868bf311b37e50a056fd16
Emmanuel Baraka <barakaemmanuel413@gmail.com>
2023-10-06 01:31:04 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Convert the response to JSON
+  })
+  .then(data => {
+    // Call the renderBooks function with the JSON data
+    renderBooks(data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 }
+
+// Call the fetchBooks function when the HTML page is loaded
+window.addEventListener('load', fetchBooks);
+  
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
